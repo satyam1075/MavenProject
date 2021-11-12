d36648380beef064bd952d83f966234571c17db9
satyam <satyam@MSI>
2021-11-12 17:13:14 +0530

    commit

---
+++ MavenProject3/src/test/java/com/TyssClient/TestScripts/xyz.java
@@ -0,0 +1,12 @@
+package com.TyssClient.TestScripts;
+
+import org.testng.annotations.Test;
+
+public class xyz 
+{
+	@Test
+	public void smomething() 
+	{
+		System.out.println("something printing");
+	}
+}
